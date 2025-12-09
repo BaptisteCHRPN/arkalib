81c2b82b733105bd23ec32dc9179e26b552d4330
Baptiste.Cherpin <cherpinb@gmail.com>
2025-12-09 10:00:11 +0100

    some stuff

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -55,7 +55,7 @@ class RegistrationController extends AbstractController
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
-        ]);
+        ]); 
     }
 
     #[Route('/verify/email', name: 'app_verify_email')]
