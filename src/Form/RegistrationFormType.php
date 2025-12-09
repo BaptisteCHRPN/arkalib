894a6683aa13a61f0e373e0676676bccbb85a9a0
Baptiste.Cherpin <cherpinb@gmail.com>
2025-12-09 15:10:47 +0100

    remmember me

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,29 +4,50 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', null, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => "Email"
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'J\'accepte les condition d\'utilisation',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter nos conditions d\'utilisation.',
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => false,
+                                     'attr' => [
+                                        'placeholder' => "Mot de passe"
+                                     ]
+                                    ],
+                'second_options' => ['label' => false,
+                                      'attr' => [
+                                        'placeholder' => 'Comfirmez le mot de passe'
+                                      ]
+                                    ],
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
