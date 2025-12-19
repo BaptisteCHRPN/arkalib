588b1c777eed4218ce3df1e069518150cab16ee0
BaptisteCHRPN <142516862+BaptisteCHRPN@users.noreply.github.com>
2025-12-19 20:44:54 +0100

    changement du nom admin controller

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\BudgetLineRepository;
+use App\Repository\OrganizationRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Config\Security\ProviderConfig\Memory\UserConfig;
 
 final class BackOfficeController extends AbstractController
 {
     #[Route('/backoffice', name: 'app_back_office')]
-    public function index(): Response
+    public function index(OrganizationRepository $organization, BudgetLineRepository $budget, UserRepository $user): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         return $this->render('back_office/index.html.twig', [
-            'controller_name' => 'BackOfficeController',
+            'organizations' => $organization,
+            'budgets' => $budget,
+            'users' => $user,
         ]);
     }
 }
