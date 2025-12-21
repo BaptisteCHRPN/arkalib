3924f56a0add317f0c90cb310559ab3c8e3c4ea2
BaptisteCHRPN <142516862+BaptisteCHRPN@users.noreply.github.com>
2025-12-21 18:41:05 +0100

    installation de bootstrap via npm

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,12 @@
-import './stimulus_bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+// assets/app.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/custom-bootstrap.scss';
+import * as bootstrap from 'bootstrap';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// Rendre Bootstrap disponible globalement
+window.bootstrap = bootstrap;
+
+// Importer votre script
+import './navbar-toggle.js';
+
+console.log('Bootstrap chargé!');
