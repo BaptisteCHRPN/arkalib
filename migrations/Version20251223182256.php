45283bd3ca9582732ac5f31fa49ce8d20df6917d
Baptiste.Cherpin <cherpinb@gmail.com>
2025-12-23 19:41:44 +0100

    Refactor budget line entity and update migrations
    Removed obsolete migration files and renamed several migration classes for consistency. Updated BudgetLine entity to make 'descrption' nullable, set default values for 'created_at' and 'is_active', and improved the BudgetLineType form with better field types and labels. Added a new MemberBudgetLineType form.

--- migrations/Version20251223182256.php
+++ migrations/Version20251223182256.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251223142325 extends AbstractMigration
+final class Version20251223182256 extends AbstractMigration
 {
     public function getDescription(): string
     {
