fc681a5537b42217614c162b381060fe6598cddf
BaptisteCHRPN <142516862+BaptisteCHRPN@users.noreply.github.com>
2026-01-01 23:19:21 +0100

    Add MemberBudgetLineController and budget line UI link
    Introduces MemberBudgetLineController with CRUD actions for budget lines. Updates MembreBudgetController route parameter for clarity and adds a button in the member budget index template to create a new budget line.

---
+++ src/Controller/MemberBudgetLineController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Budget;
+use App\Entity\BudgetLine;
+use App\Form\BudgetLineType;
+use App\Repository\BudgetLineRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/member/budgetline')]
+final class MemberBudgetLineController extends AbstractController
+{
+    #[Route(name: 'app_admin_budget_line_index', methods: ['GET'])]
+    public function index(BudgetLineRepository $budgetLineRepository): Response
+    {
+        return $this->render('admin_budget_line/index.html.twig', [
+            'budget_lines' => $budgetLineRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new/{budgetId}', name: 'app_admin_budget_line_new', methods: ['GET', 'POST'])]
+    public function new(int $budgetId, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $budget = $entityManager->getRepository(Budget::class)->find($budgetId);
+        
+        $budgetLine = new BudgetLine();
+        $form = $this->createForm(BudgetLineType::class, $budgetLine);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($budgetLine);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_budget_line_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin_budget_line/new.html.twig', [
+            'budget_line' => $budgetLine,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_budget_line_show', methods: ['GET'])]
+    public function show(BudgetLine $budgetLine): Response
+    {
+        return $this->render('admin_budget_line/show.html.twig', [
+            'budget_line' => $budgetLine,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_budget_line_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, BudgetLine $budgetLine, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(BudgetLineType::class, $budgetLine);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_budget_line_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin_budget_line/edit.html.twig', [
+            'budget_line' => $budgetLine,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_budget_line_delete', methods: ['POST'])]
+    public function delete(Request $request, BudgetLine $budgetLine, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$budgetLine->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($budgetLine);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_budget_line_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
