e65a68c32473ee06dd63555c140fa5df00ef895c
BaptisteCHRPN <142516862+BaptisteCHRPN@users.noreply.github.com>
2026-05-13 00:46:31 +0200

    activation de turbo

--- assets/app.js
+++ assets/app.js
@@ -1,29 +1,16 @@
-// assets/app.js
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/custom-bootstrap.scss';
-import * as bootstrap from 'bootstrap';
-import '@symfony/ux-turbo';
-
-// Rendre Bootstrap disponible globalement
-window.bootstrap = bootstrap;
-
-// Importer votre script
+import '@hotwired/turbo';
 import './navbar-toggle.js';
 import './popper-unabled.js';
 
-console.log('Bootstrap chargé!');
-
-// Réinitialiser les dropdowns après chaque chargement Turbo
+// Réinitialiser les dropdowns Bootstrap après navigation Turbo
 document.addEventListener('turbo:load', function() {
-    // Réinitialiser tous les dropdowns
     document.querySelectorAll('[data-bs-toggle="dropdown"]').forEach(function(dropdownToggle) {
-        new bootstrap.Dropdown(dropdownToggle);
+        new window.bootstrap.Dropdown(dropdownToggle);
     });
 });
 
 document.addEventListener('turbo:frame-load', function() {
-    // Réinitialiser tous les dropdowns
     document.querySelectorAll('[data-bs-toggle="dropdown"]').forEach(function(dropdownToggle) {
-        new bootstrap.Dropdown(dropdownToggle);
+        new window.bootstrap.Dropdown(dropdownToggle);
     });
 });
