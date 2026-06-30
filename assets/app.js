50e5b2e08daa918532f20777c908719afdd0319d
BaptisteCHRPN <142516862+BaptisteCHRPN@users.noreply.github.com>
2026-06-30 19:49:25 +0200

    Fix du bug des menus dropdown

--- assets/app.js
+++ assets/app.js
@@ -6,16 +6,3 @@ const app = startStimulusApp();
 app.register('sidebar', SidebarController);
 
 import './popper-unabled.js';
-
-// Réinitialiser les dropdowns Bootstrap après navigation Turbo
-document.addEventListener('turbo:load', function() {
-    document.querySelectorAll('[data-bs-toggle="dropdown"]').forEach(function(dropdownToggle) {
-        new window.bootstrap.Dropdown(dropdownToggle);
-    });
-});
-
-document.addEventListener('turbo:frame-load', function() {
-    document.querySelectorAll('[data-bs-toggle="dropdown"]').forEach(function(dropdownToggle) {
-        new window.bootstrap.Dropdown(dropdownToggle);
-    });
-});
