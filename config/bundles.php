eb41b6fd71c4598b2df98bed331a53acaeecfe66
BaptisteCHRPN <cherpinb@gmail.com>
2026-09-22 12:38:07 +0200

    remove toggle password bundle

--- config/bundles.php
+++ config/bundles.php
@@ -18,6 +18,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
-    Symfony\UX\TogglePassword\TogglePasswordBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
